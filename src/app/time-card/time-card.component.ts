57febebe632a4e855549074eb7ac9c7c60812021
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2023-12-04 16:45:42 -0800

    intial commit

---
+++ src/app/time-card/time-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-card',
+  templateUrl: './time-card.component.html',
+  styleUrls: ['./time-card.component.scss']
+})
+export class TimeCardComponent {
+
+}
